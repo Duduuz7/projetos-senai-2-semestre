c883cc0119a52b242460a6f05bb50ea484feac93
Eduardo <eduardo.felipe4@aluno.senai.br>
2023-09-19 14:17:47 -0300

    Iniciado projeto Event+ API CF

---
+++ Event+_CF/webapi.event+.tarde/Domains/PresencaEvento.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.event_.tarde.Domains
+{
+    [Table(nameof(PresencaEvento))]
+    public class PresencaEvento
+    {
+        [Key]
+        public Guid IdPresencaEvento { get; set; }
+
+        [Column(TypeName ="BIT")]
+        [Required(ErrorMessage ="Situação obrigatória !")]
+        public bool Situacao { get; set; }
+
+
+        //Referência tabela Usuario
+        [Required(ErrorMessage = "Usuário obrigatório !")]
+        public Guid IdUsuario { get; set; }
+
+
+        [ForeignKey(nameof(IdUsuario))]
+        public Usuario? Usuario { get; set; }
+
+
+        //Referência tabela Evento
+        [Required(ErrorMessage = "Evento obrigatório !")]
+        public Guid IdEvento { get; set; }
+
+
+        [ForeignKey(nameof(IdEvento))]
+        public Evento? Evento { get; set; }
+    }
+}
